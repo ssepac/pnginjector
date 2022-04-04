43194e6485e804d1d1ec4855cb3b17305ef6fafe
ssepac <ssepac2@illinois.edu>
2022-04-04 16:04:31 -0600

    refactors

--- main.cpp
+++ main.cpp
@@ -9,9 +9,18 @@
 
 using namespace std;
 
-int main()
+int main(int ac, char** av)
 {
-    ifstream ifs("/Users/shanesepac/Desktop/png_sample.png", ios::binary | ios::ate);
+
+    if(ac != 5) exit(-1);
+
+    CRC crcClass;
+    char* inputDir = av[1];
+    char* outputDir = av[2];
+    char* keyword = av[3];
+    char* text = av[4];
+
+    ifstream ifs(inputDir, ios::binary | ios::ate);
     ifstream::pos_type pos = ifs.tellg();
 
     std::vector<char> result(pos);
@@ -23,7 +32,10 @@ int main()
     //length is the number of bytes in the chunk data
     //CRC calculated on type code + data, not length.
     std::vector<char> chunkType = {'t', 'E', 'X', 't'};
-    std::vector<char> chunkData = {'i', 'p', 'f', 's', 0x0, 't', 'e', 's', 't'};
+    std::vector<char> chunkData = {};
+    chunkData.insert(chunkData.end(), keyword, keyword+strlen(keyword));
+    chunkData.push_back(0x0);
+    chunkData.insert(chunkData.end(), text, text + strlen(text));
 
     //Get length
     int length = chunkData.size();
@@ -31,7 +43,6 @@ int main()
     std::vector<char> chunkTypeData = chunkType;
     chunkTypeData.insert(chunkTypeData.end(), chunkData.begin(), chunkData.end());
 
-    CRC crcClass;
     unsigned long crc = crcClass.crc(reinterpret_cast<unsigned char*>(chunkTypeData.data()), chunkTypeData.size());
 
     char* chunkAllData;
@@ -41,6 +52,7 @@ int main()
     memcpy(&chunkAllData[8], chunkData.data(), length);
     memcpy(&chunkAllData[8+length], &crc, 4);
 
+    //ntohl() conversion
     std::reverse(&chunkAllData[0], &chunkAllData[0]+4);
     std::reverse(&chunkAllData[8+length], &chunkAllData[8+length]+4);
 
@@ -51,20 +63,9 @@ int main()
     result.insert(result.end()-12, std::begin(chunkAllDataVector), std::end(chunkAllDataVector));
 
     //output file
-    ofstream fout("/Users/shanesepac/Desktop/png_sample_out3.png", ios::out | ios::binary);
+    ofstream fout(outputDir, ios::out | ios::binary);
     fout.write((char*)&result[0], result.size());
     fout.close();
-
-
-/*    for (auto & element : result)
-    {
-        std::cout << std::setfill('0')
-                  << std::setw(2)
-                  << std::uppercase
-                  << std::hex << (0xFF & element) << " ";
-    }*/
-    //49 45 4E 44 (IEND CHUNK, static)
-    //CRC at end of file: AE 42 60 82
 
     return 0;
 }
